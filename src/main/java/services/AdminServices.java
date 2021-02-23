2bd950fa0216e597759e81efd889b01b66b37102
Nataliia Gavaleshko <n.gavaleshko@gmail.com>
2021-02-23 23:26:18 +0200

    My first home work

---
+++ src/main/java/services/AdminServices.java
@@ -0,0 +1,19 @@
+package services;
+
+import models.Human;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AdminServices implements Service {
+    @Override
+    public void writeUserData(Human human) {
+        try (FileWriter writer  = new FileWriter("src/main/resources/output.txt",true)){
+            writer.write(human.toString());
+            writer.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

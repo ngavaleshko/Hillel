6baa3a3eefb0c55a6f5c71b15d282843d77895b1
Nataliia Gavaleshko <n.gavaleshko@gmail.com>
2021-03-02 21:59:49 +0200

    delete space

--- src/main/java/lesson1/models/User.java
+++ src/main/java/lesson1/models/User.java
@@ -4,5 +4,4 @@ public class User extends Human {
     public User(String name, String surname, int age, String email, String password) {
         super(name, surname, age, email, password, "USER");
     }
-
 }
